c0d67f84aec638959f9e5de566d1aa928310a892
LesKukh <73469139+LesKukh@users.noreply.github.com>
2021-01-25 16:37:26 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
-
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import React from 'react'
 
 const App = () => {
   return (
     <>
+    <Header />
+    <main>
       <h1>Welcome To The ProShop</h1>
+    </main>
+    <Footer /> 
     </>
   );
 }
