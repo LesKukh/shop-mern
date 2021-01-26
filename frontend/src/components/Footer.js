9948619812a4a4a69ab019ae2b1c79d7ebaad8b2
LesKukh <73469139+LesKukh@users.noreply.github.com>
2021-01-26 16:41:30 +0200

    Update Footer.js

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Container, Row, Col } from "react-bootstrap"
 
 const Footer = () => {
   return (
-    <header>
-      footer
-    </header>
+    <Container>
+        <Row>
+            <Col className="text-center py-3">
+                Copyright &copy; ProShop
+            </Col>
+        </Row>
+    </Container>
   )
 }
 
