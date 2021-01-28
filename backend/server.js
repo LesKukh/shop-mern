6c6bb02b21f9ace542a5649ea342b9e5a73f1841
LesKukh <73469139+LesKukh@users.noreply.github.com>
2021-01-28 15:12:31 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import products from './data/products'
 import dotenv from 'dotenv.js'
 import connectDB from "./config/db.js"
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js"
 
 dotenv.config()
 
@@ -9,18 +13,18 @@ connectDB()
 
 const app = express();
 
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+
+app.use(notFound) 
+app.use(errorHandler)
+
 app.get("/", (req, res) => {
     res.send("API is running...")
 });
 
-app.get("/api/products", (req, res) => {
-    res.json(products)
-});
 
-app.get("/api/products/:id", (req, res) => {
-    const product = products.find((p) => p._id === req.params.id)
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
