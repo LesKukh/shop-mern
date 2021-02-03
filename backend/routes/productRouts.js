13b4e205870818190e7f1e7f5e5f5490d01cb840
LesKukh <73469139+LesKukh@users.noreply.github.com>
2021-02-03 12:44:13 +0200

    Update productRouts.js

--- backend/routes/productRouts.js
+++ backend/routes/productRouts.js
@@ -1,28 +1,23 @@
 import express from 'express'
-import asyncHandler from 'expressAsyncHandler'
 const router = express.Router()
-import Product from "../models/productModel"
+import {
+  getProducts,
+  getProductById,
+  deleteProduct,
+  createProduct,
+  updateProduct,
+  createProductReview,
+  getTopProducts,
+} from '../controllers/productController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
-
-//@desc Fetch all producs
-//@route GET /api/products
-//@access public
-router.get("/", asyncHandler( async(req, res) => {
-    const products = await Product.fimd({})
-    res.json(products)
-}));
-
-//@desc Fetch single producs
-//@route GET /api/products/:id
-//@access public
-router.get("/:id", asyncHandler(async(req, res) => {
-    const product = await products.findById(req.params.id)
-    if (product) {
-        res.json(product)
-    } else {
-        res.status(404).json({ message: "Product not found!" })
-    }
-    
-}))
+router.route('/').get(getProducts).post(protect, admin, createProduct)
+router.route('/:id/reviews').post(protect, createProductReview)
+router.get('/top', getTopProducts)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 export default router
