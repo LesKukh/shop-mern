9eacd3f55b19e89f3a6d8351547eab64336a077e
LesKukh <73469139+LesKukh@users.noreply.github.com>
2021-02-09 16:49:17 +0200

    Update userActions.js

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -114,4 +114,39 @@ export const logout = () => (dispatch) => {
     }
   }
 
+  export const getUserDetails = (id) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: USER_DETAILS_REQUEST,
+      })
   
+      const {
+        userLogin: { userInfo },
+      } = getState()
+  
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      }
+  
+      const { data } = await axios.get(`/api/users/${id}`, config)
+  
+      dispatch({
+        type: USER_DETAILS_SUCCESS,
+        payload: data,
+      })
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+      if (message === 'Not authorized, token failed') {
+        dispatch(logout())
+      }
+      dispatch({
+        type: USER_DETAILS_FAIL,
+        payload: message,
+      })
+    }
+  }
